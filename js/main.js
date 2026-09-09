30cd7f583ea7abba7d44b24dbd6db91e37d63f68
daehyunchoi-cell <cdh66fjqm@gmail.com>
2026-09-09 14:59:55 +0900

    Add debug log

--- js/main.js
+++ js/main.js
@@ -551,6 +551,7 @@ function handleFieldBlur(event) {
 }
 
 async function handleFormSubmit(event) {
+    console.log('폼 제출 함수 실행됨!');
     event.preventDefault();
 
     const results = FIELD_NAMES.map(validateField);
